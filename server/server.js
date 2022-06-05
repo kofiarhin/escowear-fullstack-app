913b7b7dfc63115056194b314c97e2e9b71f3544
mac <mac@esco-3.local>
2022-06-05 20:37:13 +0000

    update

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use("/api/users", usersRoute);
 
 // production setup
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     const filePath = path.resolve(
